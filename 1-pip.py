7e38742534182369edad2e4a02daaf42d4064d10
Slava.In <vyacheslav.inyutochkin@gmail.com>
2025-08-26 21:34:24 +0300

    feat: start playing with point in polygon

---
+++ 1-pip.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plt.plot(10, 0)
+# plt.plot(4, 0)
+
+plt.grid()
+plt.show()
+
+# def fun1(x):
+#     return 10*x
+
+# def fun2(x):
+#     return 5*x+20
+
+# MAX_X = 100
+# x = range(MAX_X)
+
+# y1,y2 = zip(*[ (fun1(val), fun2(val)) for val in x ])
+
+# plt.plot(x, y1)
+# plt.plot(x, y2)
+# plt.ylabel('some numbers')
+# plt.show()
